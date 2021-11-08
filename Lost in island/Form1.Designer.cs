d969d401551146724dd44e66d97e43e76891509a
P4 <P4@DESKTOP-UAB8AL1>
2021-11-08 13:07:17 -0500

    Add project files.

---
+++ Lost in island/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace Lost_in_island
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.option1label = new System.Windows.Forms.Label();
+            this.option2label = new System.Windows.Forms.Label();
+            this.option3label = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Lost_in_island.Properties.Resources.OIP;
+            this.pictureBox1.Location = new System.Drawing.Point(0, -7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(315, 213);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(24, 19);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(279, 29);
+            this.outputLabel.TabIndex = 1;
+            this.outputLabel.Text = "You are lost n a island.";
+            // 
+            // option1label
+            // 
+            this.option1label.AutoSize = true;
+            this.option1label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option1label.Location = new System.Drawing.Point(83, 224);
+            this.option1label.Name = "option1label";
+            this.option1label.Size = new System.Drawing.Size(37, 20);
+            this.option1label.TabIndex = 2;
+            this.option1label.Text = "Yes";
+            this.option1label.Click += new System.EventHandler(this.option1label_Click);
+            // 
+            // option2label
+            // 
+            this.option2label.AutoSize = true;
+            this.option2label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option2label.Location = new System.Drawing.Point(83, 259);
+            this.option2label.Name = "option2label";
+            this.option2label.Size = new System.Drawing.Size(29, 20);
+            this.option2label.TabIndex = 3;
+            this.option2label.Text = "No";
+            this.option2label.Click += new System.EventHandler(this.option2label_Click);
+            // 
+            // option3label
+            // 
+            this.option3label.AccessibleDescription = "";
+            this.option3label.AutoSize = true;
+            this.option3label.Location = new System.Drawing.Point(84, 183);
+            this.option3label.Name = "option3label";
+            this.option3label.Size = new System.Drawing.Size(139, 13);
+            this.option3label.TabIndex = 4;
+            this.option3label.Text = "Do you want to go left side?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(315, 301);
+            this.Controls.Add(this.option3label);
+            this.Controls.Add(this.option2label);
+            this.Controls.Add(this.option1label);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Lost in island";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label option1label;
+        private System.Windows.Forms.Label option2label;
+        private System.Windows.Forms.Label option3label;
+    }
+}
+
---
+++ Lost in island/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lost_in_island
+{
+    public partial class Form1 : Form
+    {
+        int page = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            outputLabel.Text = "You are lost in a island";
+            option1label.Text = "Go left";
+            option2label.Text = "Go right";
+            option3label.Text = "Which side do you want to go?";
+        }
+
+
+        private void option1label_Click(object sender, EventArgs e)
+        {
+            if (page == 1) { page = 2; }
+            else if (page == 2) { page = 6; }
+            else if (page == 6) { page = 1; }
+            else if (page == 5) { page = 8; }
+            else if (page == 8) { page = 10; }
+            else if (page == 10) { page = 1; }
+            else if (page == 9) { page = 12; }
+            else if (page == 12) { page = 1; }                   
+            else if (page == 13) { page = 1; }           
+            else if (page == 3) { page = 1; }
+
+            DisplayPage();
+        }
+
+        private void option2label_Click(object sender, EventArgs e)
+        {
+            if (page == 6) { page = 7; }
+            else if (page == 2) { page = 5; }
+            else if (page == 10) { page = 14; }
+            else if (page == 8) { page = 11; }
+            else if (page == 11) { page = 15; }
+            else if (page == 5) { page = 9; }
+            else if (page == 12) { page = 18; }
+            else if (page == 9) { page = 13; }
+            else if (page == 13) { page = 17; }
+            else if (page == 1) { page = 3; }
+            else if (page == 3) { page = 4; }
+
+            DisplayPage();
+        }
+
+        public void DisplayPage()
+        {
+            switch(page)
+            {
+                case 1:
+                    outputLabel.Text = "You are lost in a island";
+                    option1label.Text = "Go left";
+                    option2label.Text = "Go right";
+                    option3label.Text = "Which side do you want to go?";
+                    break;
+                case 2:
+                    outputLabel.Text = "You see a fruit tree Do you want to eat the fruit from the tree?";
+                    option1label.Text = "Yes";
+                    option2label.Text = "No";
+                    break;
+                case 3:
+                    outputLabel.Text = "You were trapped by savages and died. you wanna play again?";
+                    option1label.Text = "Yes";
+                    option2label.Text = "No";
+                    break;
+                case 4:
+                    outputLabel.Text = "Thank you";
+                    option1label.Text = "";
+                    option2label.Text = "";
+                    break;
+                case 5:
+                    outputLabel.Text = "A sheep past you Do you want to chase it?";
+                    option1label.Text = "Yes";
+                    option2label.Text = "No";
+                    break;
+                case 6:
+                    outputLabel.Text = "The fruit is poisonous You are dead wnna play again?";
+                    option1label.Text = "Yes";
+                    option2label.Text = "No";
+                    break;
+                case 7:
+                    outputLabel.Text = "Thank you for playing";
+                    option1label.Text = "";
+                    option2label.Text = "";
+                    break;
+                case 8:
+                    outputLabel.Text = "When you were chasing it several savages found you Do you want to escape or fight them";
+                    option1label.Text = "Yes";
+                    option2label.Text = "No";
+                    break;
+                case 9:
+                    outputLabel.Text = "You decided to look for water. After a while, you heared the sound of the waterfall Do you want to see it with the sound?";
+                    option1label.Text = "Yes";
+                    option2label.Text = "No";
+                    break;
+                case 10:
+                    outputLabel.Text = "There are too many of them.You were killed. You wanna play again?";
+                    option1label.Text = "Yes";
+                    option2label.Text = "No";
+                    break;
+                case 11:
+                    outputLabel.Text = "Running. you ran to the beach and saw your rescue team coming to pick you up. so you were saved.";
+                    option1label.Text = "";
+                    option2label.Text = "";
+                    break;
+                case 12:
+                    outputLabel.Text = "With the sound of the waterfall. You reach the place where the sound comes from only to find that it is a savage trap. and then you are killed by them. you wnna play again";
+                    option1label.Text = "Yes";
+                    option2label.Text = "No";
+                    break;
+                case 13:
+                    outputLabel.Text = "You died of dehydration because you didn't drink water for a long time play again?";
+                    option1label.Text = "yes";
+                    option2label.Text = "no";
+                    break;
+                case 14:
+                    outputLabel.Text = "Thank you for playing";
+                    option1label.Text = "";
+                    option2label.Text = "";
+                    break;
+                case 15:
+                    outputLabel.Text = "You win! game over.";
+                    option1label.Text = "";
+                    option2label.Text = "";
+                    break;
+                case 17:
+                    outputLabel.Text = "Thank you for playing";
+                    option1label.Text = "";
+                    option2label.Text = "";
+                    break;
+                case 18:
+                    outputLabel.Text = "Thank you for playing";
+                    option1label.Text = "";
+                    option2label.Text = "";
+                    break;
+                default:
+                    break;
+
+
+            }
+        }
+    }
+}
+    
+
+
